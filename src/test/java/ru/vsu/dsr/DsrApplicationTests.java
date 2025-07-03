cacb0e4ddbfe64ea5d6b28e4742df11c238ce41f
gitash2 <shapor.art@yandex.ru>
2025-07-03 17:17:38 +0300

    init commit

---
+++ src/test/java/ru/vsu/dsr/DsrApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.dsr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
